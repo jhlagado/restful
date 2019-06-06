2d06b0d4a7d1618b80b4b8a6d4a2894d47418961
John Hardy <jh@lagado.com>
2019-06-07 00:07:02 +1000

    show bad for practice

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,5 @@ module.exports = {
     "object-curly-newline": 0,
     "no-param-reassign": 0,
     "no-return-assign": 0,
-    "no-restricted-syntax": 0,
   }
 };
---
+++ ex/dupe/8async-await-for-bad/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const source = path.join(__dirname, '..', 'source');
+const dest = path.join(__dirname, 'dest');
+
+const run = async () => {
+  try {
+    await fs.access(dest);
+  } catch (err) {
+    await fs.mkdir(dest);
+  }
+  const srcFiles = await fs.readdir(source);
+
+  // BAD
+
+  for (const filename of srcFiles) {
+    const srcFilePath = path.join(source, filename);
+    const destFilePath = path.join(dest, filename);
+    const string = await fs.readFile(srcFilePath, 'utf-8');
+    await fs.writeFile(destFilePath, string + string);
+    console.log('done!');
+  };
+};
+
+run();
